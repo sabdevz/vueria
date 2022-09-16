0b6755bec2a966217f037355d5cc59e81ee272d2
sabdevz <rgsabareesh@gmail.com>
2022-09-16 16:41:15 +0530

    Basic routing and data imported to project

--- vue-router-me/src/App.vue
+++ vue-router-me/src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>Welcome to vue router</div>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
+  <div class="container">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'App',
+};
 </script>
