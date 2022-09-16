ae6a743778d54233dacd49ef44192a7b7ab6deae
sabdevz <rgsabareesh@gmail.com>
2022-09-16 14:41:14 +0530

    Application structure

--- forum-me/src/App.vue
+++ forum-me/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>App</div>
+  <div class="container">
+    <page-home />
+  </div>
 </template>
 
 <script>
+import PageHome from "./components/PageHome.vue";
+
 export default {
   name: "App",
+  components: { PageHome },
 };
 </script>
+
+<style>
+@import "assets/style.css";
+
+#app {
+  text-align: center;
+}
+</style>
