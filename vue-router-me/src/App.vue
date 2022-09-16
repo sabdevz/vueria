b8a44aa10f11e083d3a9d18be6a9a30f2cd6fbff
sabdevz <rgsabareesh@gmail.com>
2022-09-16 19:21:32 +0530

    Dynamic Ruting Started

--- vue-router-me/src/App.vue
+++ vue-router-me/src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
-  </div>
   <div class="container">
+    <the-navigation />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import TheNavigation from './components/TheNavigation.vue';
 export default {
   name: 'App',
+  components: { TheNavigation },
 };
 </script>
+
+<style>
+@import './main.css';
+</style>
--- vue-router-me/src/views/Home.vue
+++ vue-router-me/src/views/Home.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>All Destinations</h1>
+    <router-link
+      v-for="destination in destinations"
+      :to="destination.slug"
+      :key="destination.id"
+      >{{}}
+      <h2>{{ destination.name }}</h2>
+      <img :src="`/images/${destination.image}`" :alt="destination.name" />
+    </router-link>
   </div>
 </template>
 
-<script></script>
+<script>
+import sourceData from '@/data.json';
+
+export default {
+  data() {
+    return {
+      destinations: sourceData.destinations,
+    };
+  },
+};
+</script>
