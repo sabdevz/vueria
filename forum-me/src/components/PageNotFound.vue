6e4b4a758bac141f687c0e219199c4829ebef5f0
sabdevz <rgsabareesh@gmail.com>
2022-09-17 14:24:49 +0530

    Routing and 404

---
+++ forum-me/src/components/PageNotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="col-full text-center">
+    <h1>Not Found</h1>
+    <router-link to="/">Read Some Cool Articles...</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- forum-me/src/router/index.js
+++ forum-me/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import PageHome from "@/components/PageHome.vue";
 import PageThreadShow from "@/components/PageThreadShow.vue";
+import PageNotFound from "@/components/PageNotFound.vue";
+import sourceData from "@/data";
 
 const routes = [
   { path: "/", name: "Home", component: PageHome },
@@ -9,7 +11,29 @@ const routes = [
     path: "/thread/:id",
     name: "ThreadShow",
     component: PageThreadShow,
+    beforeEnter(to, from, next) {
+      // check if thread exists
+      const threadExist = sourceData.threads.find(
+        (thread) => thread.id === to.params.id
+      );
+      // if exists continue
+      if (threadExist) {
+        next();
+      } else {
+        next({
+          name: "NotFound",
+          // preserve current path and remove the first char to avoid the target URL starting with `//`
+          params: { pathMatch: to.path.substring(1).split("/") },
+          // preserve existing query and hash if any
+          query: to.query,
+          hash: to.hash,
+        });
+      }
+      // if does'nt exist  redirect to not found
+    },
   },
+  // will match everything and put it under `$route.params.pathMatch`
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: PageNotFound },
 ];
 
 const router = createRouter({
